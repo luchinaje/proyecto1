740299f4a641ec077b091c6c38f6e1cbc1486c01
luchinaje <luchinaje@gmail.com>
2022-04-15 21:04:28 -0300

    first commit

---
+++ src/main/java/com/example1/proyecto1/Proyecto1Application.java
@@ -0,0 +1,25 @@
+package com.example1.proyecto1;
+
+import com.example1.proyecto1.component.ComponentDependency;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto1Application implements CommandLineRunner {
+
+	private ComponentDependency componentDependency;
+
+	public Proyecto1Application(ComponentDependency componentDependency){
+		this.componentDependency=componentDependency;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		componentDependency.saludar();
+	}
+}
---
+++ src/main/java/com/example1/proyecto1/component/ComponentDependency.java
@@ -0,0 +1,5 @@
+package com.example1.proyecto1.component;
+
+public interface ComponentDependency {
+    void saludar();
+}
