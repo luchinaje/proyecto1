3b87f5610282a782468f7671bc29953e87748304
luchinaje <luchinaje@gmail.com>
2022-04-15 23:13:39 -0300

    ComponentTwoImplement

--- src/main/java/com/example1/proyecto1/Proyecto1Application.java
+++ src/main/java/com/example1/proyecto1/Proyecto1Application.java
@@ -1,6 +1,7 @@
 package com.example1.proyecto1;
 
 import com.example1.proyecto1.component.ComponentDependency;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +11,7 @@ public class Proyecto1Application implements CommandLineRunner {
 
 	private ComponentDependency componentDependency;
 
-	public Proyecto1Application(ComponentDependency componentDependency){
+	public Proyecto1Application(@Qualifier("componentTwoImplement") ComponentDependency componentDependency){
 		this.componentDependency=componentDependency;
 	}
 
---
+++ src/main/java/com/example1/proyecto1/component/ComponentTwoImplement.java
@@ -0,0 +1,11 @@
+package com.example1.proyecto1.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ComponentTwoImplement implements ComponentDependency{
+    @Override
+    public void saludar() {
+        System.out.println("Hola mundo desde el compnente 2");
+    }
+}
